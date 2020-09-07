f84a045b9bdef07ed5d8ae34b8f449730f7bc787
Thomas Bigelow <tjbigelow@att.net>
2020-09-07 20:01:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = "empty"
+  min_value = 10000
   key = :empty
   name_hash.each do |key, value|
-    if min_value == "empty"
+    if min_value == 10000
       min_value = value
       key = key
     end
