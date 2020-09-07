e6469329317b09c95bbf58010b8620409456805b
Thomas Bigelow <tjbigelow@att.net>
2020-09-07 20:03:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
   min_value = 10000
   key = ""
   name_hash.each do |key, value|
-    if min_value == 10000 do
+    if min_value == 10000
       min_value = value
       key = key
     end
   end
-  return key
+  key
 end
